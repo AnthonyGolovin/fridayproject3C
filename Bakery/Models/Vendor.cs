efcfd5c42b8d5c7d3ae02398a902a80f29b9fee4
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-08-02 12:14:56 -0700

    added order class, though I have had some family come over that I need to cook for so might commits might be a bit more sparse

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -33,4 +33,19 @@ namespace Bakery.Models
     }
 
   }
+    public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public string Date { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+
+     public Order (string title, string description, double price, string date)
+    {
+      Title = title;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
 }
